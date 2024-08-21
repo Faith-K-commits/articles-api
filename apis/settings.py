3f3f78bb3ef2c2ff08e2c0c711409eebdc520026
Faith Kariuki <tcwamboi@gmail.com>
2024-08-21 20:11:43 +0300

    Modify host domain

--- apis/settings.py
+++ apis/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-ld*nl&(zvcceb_=1x%tgviit5nn+ar&zxxputwxf6qu8j9hk)z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://articles-api-bc0e23cce04c.herokuapp.com/', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['articles-api-bc0e23cce04c.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
